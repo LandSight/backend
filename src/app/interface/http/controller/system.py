bb43021364335ad54a44b9ed2263d0f869ccbc42
ShavrinAleksei <apshavrinwork@gmail.com>
2025-12-27 10:13:25 +0000

    fix(api): add sync to thread false value for health sync endpoint

--- src/app/interface/http/controller/system.py
+++ src/app/interface/http/controller/system.py
@@ -3,7 +3,7 @@ from litestar import get
 from app.interface.http.schema.system import HealthResponse
 
 
-@get("/health")
+@get("/health", sync_to_thread=False)
 def health() -> HealthResponse:
     """Check service availability."""
     return HealthResponse(status="ok")
