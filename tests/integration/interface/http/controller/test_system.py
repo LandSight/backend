d6ffaf4589102d78dc09090c4b9d4c4ccc1f9867
ShavrinAleksei <apshavrinwork@gmail.com>
2025-12-29 12:34:02 +0000

    test(integration): add health check endpoint test

---
+++ tests/integration/interface/http/controller/test_system.py
@@ -0,0 +1,14 @@
+import typing
+
+from litestar import Litestar, status_codes
+
+
+if typing.TYPE_CHECKING:
+    from litestar.testing import AsyncTestClient
+
+
+async def test_health_check_endpoint(test_client: AsyncTestClient[Litestar]) -> None:
+    """Health check endpoint returns HTTP 200 and ok status."""
+    response = await test_client.get("/health")
+    assert response.status_code == status_codes.HTTP_200_OK
+    assert response.json() == {"status": "ok"}
