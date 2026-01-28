080aa4fd3c4b5da1e14b1807a642af8c7f0bdc56
Vlad Korneev <veenrok@veenrok.com>
2026-01-29 01:21:24 +0300

    feat(shared): add entity id value object

---
+++ src/app/shared/domain/value_objects/id.py
@@ -0,0 +1,25 @@
+import uuid
+from typing import Final, Literal
+
+from app.shared.domain.errors import InvalidUUIDTypeError, InvalidUUIDVersionError
+from app.shared.domain.value_objects.base import BaseValueObject
+
+
+class EntityIdValueObject(BaseValueObject[uuid.UUID]):
+    """Entity ID value object."""
+
+    _UUID_VERSION: Final[Literal[6]] = 6
+
+    @staticmethod
+    def default() -> EntityIdValueObject:
+        """Return default entity ID."""
+        return EntityIdValueObject(uuid.uuid6())
+
+    def _normalize(self, value: uuid.UUID) -> uuid.UUID:
+        return value
+
+    def _validate(self) -> None:
+        if not isinstance(self._value, uuid.UUID):
+            raise InvalidUUIDTypeError(self._value)
+        elif self._value.version != self._UUID_VERSION:
+            raise InvalidUUIDVersionError(self._value.version)
