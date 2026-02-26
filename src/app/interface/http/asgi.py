568fb432fb37f96a468b5eb08b3cfe235c915df8
ShavrinAleksei <apshavrinwork@gmail.com>
2026-02-26 13:38:50 +0300

    feat(interface): init asgi app

---
+++ src/app/interface/http/asgi.py
@@ -0,0 +1,34 @@
+from litestar import Litestar
+from litestar.openapi import OpenAPIConfig
+from litestar.openapi.plugins import ScalarRenderPlugin
+from litestar.openapi.spec import License
+
+from app import __api_version__
+from app.interface.http.controller.system import SystemController
+from app.platform.config.loaders import load_logging_config
+from app.platform.logging import configure_logging
+
+
+def create_asgi_application() -> Litestar:
+    """Create and configure the ASGI application.
+
+    Returns
+    -------
+    Litestar
+        ASGI application.
+    """
+    app = Litestar(
+        route_handlers=[
+            SystemController,
+        ],
+        openapi_config=OpenAPIConfig(
+            title="Land Sight API",
+            version=__api_version__,
+            license=License(name="MIT", url="https://opensource.org/licenses/MIT"),
+            render_plugins=[ScalarRenderPlugin()],
+        ),
+    )
+    logging_config = load_logging_config()
+    configure_logging(config=logging_config)
+
+    return app
