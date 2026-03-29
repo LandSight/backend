286fc02a3ce228b3802f40bd0407f66a18613f61
Vlad Korneev <veenrok@veenrok.com>
2026-03-29 23:58:28 +0300

    feat: add abstract base value object

---
+++ src/app/module/shared/domain/value_object/base.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from copy import deepcopy
+
+
+class BaseValueObject[ValueT](ABC):
+    """Base class for all value objects."""
+
+    def __init__(self, value: ValueT) -> None:
+        self._value = self._normalize(value)
+        self._validate()
+
+    @abstractmethod
+    def _normalize(self, value: ValueT) -> ValueT:
+        """Return the normalized value."""
+
+    @abstractmethod
+    def _validate(self) -> None:
+        """Validate the value."""
+
+    def unwrap(self) -> ValueT:
+        """Return the underlying value."""
+        return deepcopy(self._value)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self._value == other._value
+
+    def __hash__(self) -> int:
+        return hash((self.__class__.__name__, self._value))
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self._value!r})"
+
+
+__all__ = ("BaseValueObject",)
