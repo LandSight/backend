81b68305b6bc6e861e9fccddf0fe154b6042f488
Шаврин Алексей <shavrin.ap@devzone.local>
2026-07-14 13:11:37 +0300

    feat(parcel): add owner id value object

--- src/app/module/parcel/domain/value_object/__init__.py
+++ src/app/module/parcel/domain/value_object/__init__.py
@@ -1,6 +1,7 @@
 from .geo_point import GeoPoint
 from .latitude import Latitude
 from .longitude import Longitude
+from .owner_id import OwnerId
 from .parcel_id import ParcelId
 from .parcel_name import ParcelName
 from .polygon import Polygon
@@ -10,6 +11,7 @@ __all__ = (
     "GeoPoint",
     "Latitude",
     "Longitude",
+    "OwnerId",
     "ParcelId",
     "ParcelName",
     "Polygon",
