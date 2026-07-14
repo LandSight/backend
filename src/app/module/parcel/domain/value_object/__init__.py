1bb94017a441bcb9a8d3095e9c3afba4c574d346
Шаврин Алексей <shavrin.ap@devzone.local>
2026-07-14 11:01:50 +0300

    feat(parcel): update init for value objects

---
+++ src/app/module/parcel/domain/value_object/__init__.py
@@ -0,0 +1,16 @@
+from .geo_point import GeoPoint
+from .latitude import Latitude
+from .longitude import Longitude
+from .parcel_id import ParcelId
+from .parcel_name import ParcelName
+from .polygon import Polygon
+
+
+__all__ = (
+    "GeoPoint",
+    "Latitude",
+    "Longitude",
+    "ParcelId",
+    "ParcelName",
+    "Polygon",
+)
