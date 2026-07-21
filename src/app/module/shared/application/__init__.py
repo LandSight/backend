d21abeba4e38dce219f02ad2b405ab4a4da80e6a
Шаврин Алексей <shavrin.ap@devzone.local>
2026-07-21 15:42:08 +0300

    feat(shared): remove http interface

--- src/app/module/shared/application/__init__.py
+++ src/app/module/shared/application/__init__.py
@@ -1,8 +1,7 @@
-from . import error, port, use_case
+from . import error, use_case
 
 
 __all__ = (
     "error",
-    "port",
     "use_case",
 )
--- src/app/module/shared/application/dto/response/__init__.py
+++
@@ -1,4 +0,0 @@
-from .current_user import CurrentUser
-
-
-__all__ = ("CurrentUser",)
