4dd08077f1c48e77e0ecb961ae5ff7a9b1e1781f
ShavrinAleksei <apshavrinwork@gmail.com>
2026-09-22 11:34:36 +0300

    feat(infrastructure): add new geometry type support and tags field

--- src/app/module/infrastructure/domain/entity/__init__.py
+++ src/app/module/infrastructure/domain/entity/__init__.py
@@ -1,18 +1,18 @@
-from .hospital_metrics import HospitalMetrics
+from .ecology_metrics import EcologyMetrics
+from .facility_metrics import FacilityMetrics
+from .geographic_position_metrics import GeographicPositionMetrics
 from .infrastructure_metrics import InfrastructureMetrics
 from .infrastructure_object import InfrastructureObject
-from .school_metrics import SchoolMetrics
-from .shop_metrics import ShopMetrics
-from .transit_stop_metrics import TransitStopMetrics
-from .water_body_metrics import WaterBodyMetrics
+from .road_accessibility_metrics import RoadAccessibilityMetrics
+from .utility_metrics import UtilityMetrics
 
 
 __all__ = (
-    "HospitalMetrics",
+    "EcologyMetrics",
+    "FacilityMetrics",
+    "GeographicPositionMetrics",
     "InfrastructureMetrics",
     "InfrastructureObject",
-    "SchoolMetrics",
-    "ShopMetrics",
-    "TransitStopMetrics",
-    "WaterBodyMetrics",
+    "RoadAccessibilityMetrics",
+    "UtilityMetrics",
 )
